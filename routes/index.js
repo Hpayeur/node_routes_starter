2330174686febba9f5563bd78e5f4c3e008885bb
Hpayeur <hailpaye017@gmail.com>
2026-02-25 16:49:59 -0700

    Finished

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const myController = require("../controllers");
 const routes = require("express").Router();
 
 routes.get("/", myController.awesomeFunction);
-
-routes.get("/ttech", myController.tooeleTech);
+routes.get("/ttech", myController.tooeleTechFunction);
+routes.get("/students", myController.getAllStudents);
 
 module.exports = routes;
